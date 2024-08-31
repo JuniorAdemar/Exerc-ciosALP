1e80e0d99923d0b6188ffa4fe45bd91f3bfac829
Ademar Simon Júnior <ademarsimonjunior203@gmail.com>
2024-08-31 12:25:45 -0300

    exercício 1 feito

---
+++ Ex1/Ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string nome = "abacate";
+
+    cout <<"Qual seu nome?"<< endl;
+
+    cin >> nome;
+
+    cout <<"Seja bem vindo "<< nome <<"!!!"<< endl;
+
+}
