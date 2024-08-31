4abf2b9c88e1d731d798f43eed11061ab7626022
Ademar Simon Júnior <ademarsimonjunior203@gmail.com>
2024-08-31 13:01:08 -0300

    Exercício 2 feito

--- Ex1/Ex1.cpp
+++ Ex1/Ex1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    string nome = "abacate";
+    string nome = "";
 
     cout <<"Qual seu nome?"<< endl;
 
---
+++ Ex2/Ex2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    char alternativa;
+    char correta = 'a';
+
+    cout <<"Qual é o verdadeiro nome do super-herói Batman?"<< endl;
+    cout <<"a) Bruce Wayne"<< endl;
+    cout <<"b) Clark Kent"<< endl;
+    cout <<"c) Peter Parker"<< endl;
+    cout <<"d) Tony Stark"<< endl;
+    cout <<"e) Steve Rogers"<< endl;
+
+    cin >> alternativa;
+
+    if(alternativa == correta){
+
+        cout <<"Voce respondeu alternativa "<<alternativa<<", ela esta correta!!"<<endl;
+
+    }else{
+        cout <<"Voce respondeu alternativa "<<alternativa<<", a correta eh a alternativa "<<correta<<endl;
+    }
+
+}
