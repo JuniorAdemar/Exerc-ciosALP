afc39e166573b8bd563e5c88615794e2b434405b
Ademar Simon Junior <JuniorAdemar@edu.unisinos.br>
2024-09-11 21:30:24 -0300

    correção nas mensagens do exercicio 10

--- Lista 3 (31.08.24)/Ex10/Ex10.cpp
+++ Lista 3 (31.08.24)/Ex10/Ex10.cpp
@@ -8,46 +8,52 @@ de cada moeda em relação ao real. Depois apresente o seguinte menu:
 6) Converter de Dólar para Real
 Leia o valor a ser convertido na moeda de origem e imprima na tela a quantidade na moeda
 destino.*/
+
 #include <iostream>
-using namespace std;
 
-void convert(int opt, float x, float real, float dolar, float euro)
+void convert(int opt, float x, float dolar, float euro)
 {
-float result;
-using namespace std;
-
+    float result;
+    using namespace std;
     string result_messege = "A conversao de ";
+    string messege = "Digite o valor para conversao: ";
     string conversion_messege;
 
     switch(opt){
         case 1:
+            cout<<messege<<endl;
             cin>>x;
             result = x*euro;
             conversion_messege = "Real para Euro eh: ";
             break;
         case 2:
+            cout<<messege<<endl;
             cin>>x;
             result = x*dolar;
             conversion_messege = "Real para Dolar eh: ";
             break;
         case 3:
+            cout<<messege<<endl;
             cin>>x;
-            result = x*dolar;
+            result = x*(euro/dolar);
             conversion_messege = "Euro para Dolar eh: ";
             break;
         case 4:
+            cout<<messege<<endl;
             cin>>x;
-            result = x*real;
+            result = x/euro;
             conversion_messege = "Euro para Real eh: ";
             break;
         case 5:
+            cout<<messege<<endl;
             cin>>x;
-            result = x*euro;
+            result = x*(dolar/euro);
             conversion_messege = "Dolar para Euro eh: ";
             break;   
         case 6:
+            cout<<messege<<endl;
             cin>>x;
-            result = x*real;
+            result = x/dolar;
             conversion_messege = "Dolar para Real eh: ";
             break;
     }
@@ -57,16 +63,15 @@ using namespace std;
 }
 
 int main(){
+    using namespace std;
+
     string messege1 = "Digite o valor da cotacao do real";
     string messege2 = "Digite o valor da cotacao do dolar";
     string messege3 = "Digite o valor da cotacao do euro";
     string messege4 = "Digite a opcao desejada: \n1) Converter de Real para Euro\n2) Converter de Real para Dólar\n3) Converter de Euro para Dólar\n4) Converter de Euro para Real\n5) Converter de Dólar para Euro\n6) Converter de Dólar para Real";
 
     int option;
-    float value, real_cotation, dolar_cotation, euro_cotation;
-
-    cout<<messege1<<endl;
-    cin>>real_cotation;
+    float value, dolar_cotation, euro_cotation;
 
     cout<<messege2<<endl;
     cin>>dolar_cotation;
@@ -77,5 +82,5 @@ int main(){
     cout<<messege4<<endl;
     cin>>option;
 
-    convert(option, value, real_cotation, dolar_cotation, euro_cotation);
+    convert(option, value, dolar_cotation, euro_cotation);
 }
